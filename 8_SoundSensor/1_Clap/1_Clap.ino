d52a5536ade538b6bdce7655c8176ea852d7c5e6
KingOfPython <kimnayer@gmail.com>
2019-12-08 21:20:00 +0900

    사운드 센서 이용 박수 소리 감지하기 추가

---
+++ 8_SoundSensor/1_Clap/1_Clap.ino
@@ -0,0 +1,39 @@
+int soundSensor = A0;     // 사운드 센서 핀
+int led[6] = {8, 9, 10, 11};
+int threshold = 500; // 소리 크기 기준
+int count = 0;      // 박수를 몇 번 쳤는지 카운트
+int onOffC = 0;      // onOff 제어를 위한 카운트 변수
+
+void setup() {               
+  Serial.begin(9600); // 시리얼모니터 출력
+  pinMode(soundSensor, INPUT);  // 사운드 센서 입력으로 설정  
+  for(int i=0; i<6; i++)      // LED 핀 출력으로 설정
+    pinMode(led[i], OUTPUT);    
+}
+
+void loop() {     
+  if( analogRead(soundSensor) >= threshold){    // 감지된 소리의 크기가 500 이상일 때
+    if(onOffC % 2 == 0)
+    {
+        for(int i=0; i<4; i++){ // 4개의 led 제어
+          if(i == count % 4)        // 카운트 된 박수 개수로 led 제어 
+            digitalWrite(led[i], HIGH);   
+          else 
+            digitalWrite(led[i], LOW);
+        }
+        count++;
+        onOffC++;
+    }else
+    {
+        for(int i=0; i<4; i++){
+          digitalWrite(led[i], LOW);
+        }
+        onOffC++;
+    }
+
+    Serial.println(String("Count : ")+count);
+    delay(200);     // 박수 중복 입력이 안되게 200ms 딜레이
+  } 
+  else       // 감지된 소리의 크기가 500 이하일 때 
+    delay(1);
+}
